b0aeb895d0f2a0c0ae61c94a691faac562373173
metagamersuniversE <metagamersuniverse@gmail.com>
2023-04-21 20:58:39 +0600

    sale start

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar'
-import Cover from '../components/Cover'
+import Cover from '../components/Cover3'
 import Link from 'next/link'
 
 export default function Home() {
